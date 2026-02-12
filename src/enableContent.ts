3f4cf76d878df36f5d62470898b7db3524d800be
matt-newhall <matt@newhall.dev>
2026-02-12 23:55:59 +0000

    refactor: make extension async

--- src/enableContent.ts
+++ src/enableContent.ts
@@ -1,23 +1,26 @@
-const deleteOverlay = (selector: string, maxAttempts = 3, delay = 200) => {
-  let attempts = 0
+const deleteOverlay = (selector: string, maxAttempts = 3, delay = 200): Promise<void> => {
+  return new Promise((resolve, reject) => {
+    let attempts = 0
 
-  const attemptUntilDeleteOrBackoff = () => {
-    attempts++
-    const element = document.querySelector(selector)
+    const attemptUntilDeleteOrBackoff = () => {
+      attempts++
+      const element = document.querySelector(selector)
 
-    if (element) {
-      console.log(`Found element on attempt: ${attempts}`)
-      element.remove()
-      return
-    } else if (attempts < maxAttempts) {
-      setTimeout(attemptUntilDeleteOrBackoff, delay)
-      delay *= 2
-    }  else {
-      console.error(`Failed to find overlay element within ${maxAttempts} attempts`)
+      if (element) {
+        console.log(`Found element on attempt: ${attempts}`)
+        element.remove()
+        resolve()
+      } else if (attempts < maxAttempts) {
+        setTimeout(attemptUntilDeleteOrBackoff, delay)
+        delay *= 2
+      } else {
+        reject(new Error(`Failed to find overlay element within ${maxAttempts} attempts`))
+      }
     }
-  }
 
-  attemptUntilDeleteOrBackoff()
+    attemptUntilDeleteOrBackoff()
+  })
+
 }
 
 const unhideClues = () => {
@@ -27,4 +30,7 @@ const unhideClues = () => {
 }
 
 deleteOverlay(".xwd__modal--wrapper")
-unhideClues()
+  .then(() => {
+    unhideClues()
+  })
+  .catch(err => console.error(err))
