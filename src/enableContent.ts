bf7fa8163a028b5eabdd91ae4bf13ed86e2a1a2e
matt-newhall <matt@newhall.dev>
2026-02-13 01:07:55 +0000

    feat: enter and remove letters

--- src/enableContent.ts
+++ src/enableContent.ts
@@ -34,9 +34,50 @@ const unobscureBanner = () => {
   element?.classList.remove("obscured")
 }
 
+const makePlayable = () => {
+  document.addEventListener('keydown', (e) => {
+    if (e.key.match(/^[a-zA-Z]$/)) {
+      const activeCell = document.activeElement
+      if (activeCell?.getAttribute('role') === 'cell') {
+        // bit of a hack here to get the 'active' cell, it just happens to have this font size
+        const parentText = activeCell.parentElement?.querySelector('text[font-size="66.67"]')
+
+        if (parentText) {
+          const hiddenText = parentText.querySelector('.xwd__cell--hidden')
+
+          if (hiddenText) {
+            // delete previously entered letter
+            Array.from(parentText.childNodes).forEach(node => {
+              if (node.nodeType === Node.TEXT_NODE) {
+                node.remove()
+              }
+            })
+
+            parentText.appendChild(document.createTextNode(e.key.toUpperCase()))
+          }
+        }
+      }
+    } else if (e.key === "Backspace" || e.key === "Delete") {
+      const activeCell = document.activeElement
+      if (activeCell?.getAttribute('role') === 'cell') {
+        const parentText = activeCell.parentElement?.querySelector('text[font-size="66.67"]')
+
+        if (parentText) {
+          Array.from(parentText.childNodes).forEach(node => {
+            if (node.nodeType === Node.TEXT_NODE) {
+              node.remove()
+            }
+          })
+        }
+      }
+    }
+  })
+}
+
 deleteOverlay(".xwd__modal--wrapper")
   .then(() => {
     unhideClues()
     unobscureBanner()
+    makePlayable()
   })
   .catch(err => console.error(err))
